3bc75b1d6f703b30a99eb0d21cea55fc71b0c12d
dkozak <dkozak94@gmail.com>
2018-07-25 19:18:11 +0200

    fix bug with inner sets not being formated correctly

--- src/io/dkozak/formatextractor/SetToStringConverter.java
+++ src/io/dkozak/formatextractor/SetToStringConverter.java
@@ -13,43 +13,46 @@ import java.util.Map;
 public class SetToStringConverter extends SetBaseListener {
 
     public static String convert(MySet mySet, Map<MapKey, List<FormatInfo>> formatInfo) {
-        return convert(new ArrayList<>(), mySet, formatInfo);
+        return convert(new ArrayList<>(), 0, mySet, formatInfo);
     }
 
-    private static String convert(List<String> context, MySet mySet, Map<MapKey, List<FormatInfo>> formatInfo) {
+    private static String convert(List<String> context, int indentation, MySet mySet, Map<MapKey, List<FormatInfo>> formatInfo) {
         if (mySet instanceof SetElement)
-            return convertSetElem((SetElement) mySet);
+            return convertSetElem(indentation, (SetElement) mySet);
         else if (mySet instanceof InnerMySet) {
             if (!context.isEmpty()) {
                 context.add("elem");
             }
             InnerMySet set = (InnerMySet) mySet;
             if (set.size() == 0) {
-                return convertEmptySet(context, formatInfo);
+                return convertEmptySet(context, indentation, formatInfo);
             } else {
-                return convertNonEmptySet(context, formatInfo, set);
+                return convertNonEmptySet(context, indentation, formatInfo, set);
             }
         } else throw new UnsupportedOperationException("Unknown MySetSubtype: " + mySet.getClass()
                                                                                        .getName());
     }
 
-    private static String convertSetElem(SetElement mySet) {
-        return mySet.getName();
+    private static String convertSetElem(int indentation, SetElement mySet) {
+        StringBuilder stringBuilder = new StringBuilder();
+        appendChar(' ', stringBuilder, indentation);
+        stringBuilder.append(mySet.getName());
+        return stringBuilder.toString();
     }
 
-    private static String convertNonEmptySet(List<String> context, Map<MapKey, List<FormatInfo>> formatInfo, InnerMySet set) {
+    private static String convertNonEmptySet(List<String> context, int indentation, Map<MapKey, List<FormatInfo>> formatInfo, InnerMySet set) {
         context.add("set");
         List<FormatInfo> formatInfoList = formatInfo.get(new MapKey(context, "nonEmptySet"));
         StringBuilder stringBuilder = new StringBuilder();
+        appendChar(' ', stringBuilder, indentation);
         stringBuilder.append('{');
         appendChar('\n', stringBuilder, formatInfoList.get(0).appendNewLines);
-        int childrenIndentation = formatInfoList.get(0).childrenIndentation;
+        int childrenIndentation = indentation + formatInfoList.get(0).childrenIndentation;
 
         ArrayList<MySet> setArrayList = new ArrayList<>(set.getElements());
         for (int j = 0; j < setArrayList.size(); j++) {
             MySet element = setArrayList.get(j);
-            appendChar(' ', stringBuilder, childrenIndentation);
-            stringBuilder.append(convert(new ArrayList<>(context), element, formatInfo));
+            stringBuilder.append(convert(new ArrayList<>(context), childrenIndentation, element, formatInfo));
             appendChar('\n', stringBuilder, formatInfoList.get(1).appendNewLines);
             if (formatInfoList.get(1).appendNewLines > 0) {
                 appendChar(' ', stringBuilder, childrenIndentation);
@@ -61,18 +64,25 @@ public class SetToStringConverter extends SetBaseListener {
                 appendChar('\n', stringBuilder, formatInfoList.get(2).appendNewLines);
             }
         }
-        appendChar('\n', stringBuilder, formatInfoList.get(formatInfo.size() - 1).appendNewLines);
+        appendChar('\n', stringBuilder, formatInfoList.get(formatInfoList.size() - 1).appendNewLines);
+        if (formatInfoList.get(formatInfoList.size() - 1).appendNewLines > 0) {
+            appendChar(' ', stringBuilder, indentation);
+        }
         stringBuilder.append('}');
         return stringBuilder.toString();
     }
 
-    private static String convertEmptySet(List<String> context, Map<MapKey, List<FormatInfo>> formatInfo) {
+    private static String convertEmptySet(List<String> context, int indentation, Map<MapKey, List<FormatInfo>> formatInfo) {
         context.add("set");
         FormatInfo firstToSecondBracketRule = formatInfo.get(new MapKey(context, "emptySet"))
                                                         .get(0);
         StringBuilder stringBuilder = new StringBuilder();
+        appendChar(' ', stringBuilder, indentation);
         stringBuilder.append('{');
         appendChar('\n', stringBuilder, firstToSecondBracketRule.appendNewLines);
+        if (firstToSecondBracketRule.appendNewLines > 0) {
+            appendChar(' ', stringBuilder, indentation);
+        }
         stringBuilder.append('}');
         return stringBuilder.toString();
     }
